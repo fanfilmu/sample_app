df2f752e3fdf92f3369a0838a2b44c54c58c7347
fanfilmu <michalb@student.agh.edu.pl>
2014-07-20 16:16:10 +0200

    Finish layout

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'Static pages' do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should_not have_title('| Home') }
+  end
+
+  describe 'Help page' do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title("#{base_title} | Help") }
+  end
+
+  describe 'About page' do
+    before { visit about_path }
+
+    it { should have_content('About Us') }
+    it { should have_title("#{base_title} | About Us") }
+  end
+
+  describe 'Contact page' do
+    before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title("#{base_title} | Contact") }
+  end
+end
